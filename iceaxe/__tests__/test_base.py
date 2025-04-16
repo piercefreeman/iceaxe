a01ea6d08fc2feaaea37c7f20326c6da975af816
Pierce Freeman <piercefreeman@gmail.com>
2025-04-16 15:24:44 -0700

    Call explicit getattr instead of superclass to avoid recursion (#79)

--- iceaxe/__tests__/test_base.py
+++ iceaxe/__tests__/test_base.py
@@ -4,6 +4,7 @@ from iceaxe.base import (
     DBModelMetaclass,
     TableBase,
 )
+from iceaxe.field import DBFieldInfo
 
 
 def test_autodetect():
@@ -30,3 +31,22 @@ def test_not_autodetect_generic(clear_registry):
         pass
 
     assert DBModelMetaclass.get_registry() == [WillAutodetect]
+
+
+def test_model_fields():
+    class User(TableBase):
+        id: int
+        name: str
+
+    # Check the main fields
+    assert isinstance(User.model_fields["id"], DBFieldInfo)
+    assert User.model_fields["id"].annotation == int  # noqa: E721
+    assert User.model_fields["id"].is_required() is True
+
+    assert isinstance(User.model_fields["name"], DBFieldInfo)
+    assert User.model_fields["name"].annotation == str  # noqa: E721
+    assert User.model_fields["name"].is_required() is True
+
+    # Check that the special fields exist with the right types
+    assert isinstance(User.model_fields["modified_attrs"], DBFieldInfo)
+    assert isinstance(User.model_fields["modified_attrs_callbacks"], DBFieldInfo)
